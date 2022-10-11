7f2edaaf44342b6e77e8b78f0a93fbfb458afa87
Alan <gongchengra@gmail.com>
2022-10-11 13:03:30 +0000

    add files

--- 01.basics/16-String-Manipulation/String.go
+++ 01.basics/16-String-Manipulation/String.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"log"
+	"os"
+	"sort"
 	"strings"
+	"unicode"
 )
 
 func main() {
@@ -66,6 +71,113 @@ func main() {
 		fmt.Println(s)
 		for _, c := range s {
 			fmt.Printf("%c %[1]d\n", c)
+		}
+	}
+	{
+		var buf bytes.Buffer
+		buf.WriteString("an ")
+		buf.WriteString("old ")
+		buf.WriteString("falcon")
+		fmt.Println(buf.String())
+		w := "falcon"
+		fmt.Println(strings.Repeat(w+" ", 5))
+	}
+	{
+		w1 := "falcon"
+		w2 := "Falcon"
+		if strings.Compare(w1, w2) == 0 {
+			fmt.Println("The words are equal")
+		} else {
+			fmt.Println("The words are not equal")
+		}
+		if strings.EqualFold(w1, w2) {
+			fmt.Println("The words are equal")
+		} else {
+			fmt.Println("The words are not equal")
+		}
+		msg := "I saw a fox in the forest. The fox had brown fur."
+		output := strings.Replace(msg, "fox", "wolf", 2)
+		fmt.Println(output)
+		output2 := strings.ReplaceAll(msg, "fox", "wolf")
+		fmt.Println(output2)
+		fmt.Println(strings.Index(msg, "fox"))
+		fmt.Println(strings.LastIndex(msg, "fox"))
+	}
+	{
+		msg := "and old falcon"
+		msg2 := "čerešňa"
+		fmt.Println(strings.Title(msg))
+		fmt.Println(strings.ToUpper(msg))
+		fmt.Println(strings.ToUpper(msg2))
+		fmt.Println(strings.Title(msg2))
+	}
+	{
+		msg := "a blue 鱼"
+		r := '鱼'
+		if strings.ContainsRune(msg, r) {
+			fmt.Println("yes")
+		} else {
+			fmt.Println("no")
+		}
+		fmt.Println("-----------------")
+		if strings.Contains(msg, "鱼") {
+			fmt.Println("yes")
+		} else {
+			fmt.Println("no")
+		}
+	}
+	{
+		msg := ".an old falcon!"
+		cutset := ".!"
+		msg2 := strings.Trim(msg, cutset)
+		fmt.Println(msg2)
+		msg3 := strings.TrimLeft(msg, cutset)
+		fmt.Println(msg3)
+		msg4 := strings.TrimRight(msg, cutset)
+		fmt.Println(msg4)
+	}
+	{
+		msg := "\t\tand old falcon\n"
+		fmt.Println(msg)
+		msg2 := strings.TrimSpace(msg)
+		fmt.Println(msg2)
+		msg3 := strings.TrimFunc(msg, func(r rune) bool { return !unicode.IsLetter(r) })
+		fmt.Println(msg3)
+	}
+	{
+		msg := "--and old falcon--"
+		msg2 := strings.TrimPrefix(msg, "--")
+		fmt.Println(msg2)
+		msg3 := strings.TrimSuffix(msg, "--")
+		fmt.Println(msg3)
+	}
+	{
+		// wget https://raw.githubusercontent.com/janbodnar/data/main/the-king-james-bible.txt -O the-king-james-bible.log
+		fileName := "the-king-james-bible.log"
+		bs, err := os.ReadFile(fileName)
+		if err != nil {
+			log.Fatal(err)
+		}
+		text := string(bs)
+		fields := strings.FieldsFunc(text, func(r rune) bool {
+			return !('a' <= r && r <= 'z' || 'A' <= r && r <= 'Z' || r == '\'')
+		})
+		wordsCount := make(map[string]int)
+		for _, field := range fields {
+			wordsCount[field]++
+		}
+		keys := make([]string, 0, len(wordsCount))
+		for key := range wordsCount {
+			keys = append(keys, key)
+		}
+		sort.Slice(keys, func(i, j int) bool {
+			return wordsCount[keys[i]] > wordsCount[keys[j]]
+		})
+		for idx, key := range keys {
+			fmt.Printf("%s %d\n", key, wordsCount[key])
+			if idx == 10 {
+				break
+			}
 		}
 	}
 }
