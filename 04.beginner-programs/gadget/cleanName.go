531af2e3b82e32c7141d19ee35fa9533e51f38c1
Alan <gongchengra@gmail.com>
2022-12-03 11:05:08 +0000

    update cleanName to check existance of file

--- 04.beginner-programs/gadget/cleanName.go
+++ 04.beginner-programs/gadget/cleanName.go
@@ -7,15 +7,27 @@ import (
 )
 
 func main() {
-	oldname := "hello, 世界"
+	oldname := ""
 	if len(os.Args) > 1 {
 		oldname = os.Args[1]
+	} else {
+		fmt.Println("Please provide the filename.")
+		return
 	}
-	newname := ""
-	for _, runeValue := range oldname {
-		if unicode.Is(unicode.Han, runeValue) || unicode.IsLetter(runeValue) || unicode.IsLetter(runeValue) || runeValue == '.' {
-			newname += string(runeValue)
+	_, err := os.Open(oldname)
+	if err == nil {
+		newname := ""
+		for _, runeValue := range oldname {
+			if unicode.Is(unicode.Han, runeValue) || unicode.IsNumber(runeValue) || unicode.IsLetter(runeValue) || runeValue == '.' {
+				newname += string(runeValue)
+			}
 		}
+		fmt.Println("Renamed ", oldname, " to ", newname)
+		e := os.Rename(oldname, newname)
+		if e != nil {
+			fmt.Println(e)
+		}
+	} else {
+		fmt.Println(err)
 	}
-	fmt.Println(newname)
 }
