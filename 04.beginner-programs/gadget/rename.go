50492e9165d173bb23af35f6946ef7ab47b08c0c
Alan <gongchengra@gmail.com>
2023-06-01 08:36:37 +0000

    update rename

--- 04.beginner-programs/gadget/rename.go
+++ 04.beginner-programs/gadget/rename.go
@@ -32,17 +32,19 @@ func main() {
 	if dirs, err := f.Readdir(-1); err == nil {
 		for _, d := range dirs {
 			filename := d.Name()
+			newname := ""
+			for _, runeValue := range filename {
+				if unicode.Is(unicode.Han, runeValue) || unicode.IsNumber(runeValue) || unicode.IsLetter(runeValue) || runeValue == '.' || runeValue == '_' || runeValue == '-' {
+					newname += string(runeValue)
+				}
+			}
+			ext := filepath.Ext(newname)
+			file := strings.TrimSuffix(newname, ext)
 			match, _ := regexp.Match(`^[0-9a-zA-Z]`, []byte(filename))
 			if match == false {
-				newname := ""
-				for _, runeValue := range filename {
-					if unicode.Is(unicode.Han, runeValue) || unicode.IsNumber(runeValue) || unicode.IsLetter(runeValue) || runeValue == '.' {
-						newname += string(runeValue)
-					}
-				}
-				ext := filepath.Ext(newname)
-				file := strings.TrimSuffix(newname, ext)
-				newname = strToRuneSumString(file) + ext
+				newname = strToRuneSumString(file) + file + ext
+			}
+			if filename != newname {
 				fmt.Println("Renamed ", filename, " to ", newname)
 				e := os.Rename(filename, newname)
 				if e != nil {
