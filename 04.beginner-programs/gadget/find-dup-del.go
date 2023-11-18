f90dfa3ad1b5fa7af40892b8ced216c07a03ad3d
Alan <gongchengra@gmail.com>
2023-11-18 06:38:45 +0000

    add an option to delete all

--- 04.beginner-programs/gadget/find-dup-del.go
+++ 04.beginner-programs/gadget/find-dup-del.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	"bufio"
 	"crypto/md5"
+	"encoding/hex"
 	"fmt"
 	"io"
+	"io/fs"
 	"os"
 	"path/filepath"
-	"strings"
+	"strconv"
 )
 
 func main() {
@@ -15,93 +16,113 @@ func main() {
 		fmt.Printf("Usage: %s /path/to/your/folder\n", os.Args[0])
 		os.Exit(1)
 	}
+
 	folderPath := os.Args[1]
-	fileHashes := make(map[string][]string)
-	err := filepath.Walk(folderPath, func(path string, info os.FileInfo, err error) error {
+	hashes := make(map[string][]string)
+
+	err := filepath.WalkDir(folderPath, func(path string, d fs.DirEntry, err error) error {
 		if err != nil {
 			return err
 		}
-		if !info.IsDir() {
-			file, err := os.Open(path)
+		if !d.IsDir() {
+			hash, err := fileMD5(path)
 			if err != nil {
 				return err
 			}
-			defer file.Close()
-			hash := md5.New()
-			if _, err := io.Copy(hash, file); err != nil {
-				return err
-			}
-			hashInBytes := hash.Sum(nil)
-			hashString := fmt.Sprintf("%x", hashInBytes)
-			fileHashes[hashString] = append(fileHashes[hashString], path)
+			hashes[hash] = append(hashes[hash], path)
 		}
 		return nil
 	})
 	if err != nil {
-		fmt.Println("Error walking through files:", err)
+		fmt.Println("Error walking through directory:", err)
 		os.Exit(2)
 	}
+
 	duplicatesFound := false
 	fmt.Println("=== Duplicate file groups ===")
-	for _, files := range fileHashes {
+	for hash, files := range hashes {
 		if len(files) > 1 {
 			duplicatesFound = true
-			fmt.Printf("Group of files with hash %x:\n", md5.Sum([]byte(files[0])))
+			fmt.Printf("Group of files with hash %s:\n", hash)
 			for _, file := range files {
 				fmt.Println(file)
 			}
 			fmt.Println()
 		}
 	}
+
 	if !duplicatesFound {
 		fmt.Println("No duplicate files found.")
 		os.Exit(0)
 	}
-	reader := bufio.NewReader(os.Stdin)
+
+	var deleteChoice string
 	fmt.Print("Would you like to delete duplicates, keeping only one copy of each file? (yes/no) ")
-	deleteChoice, _ := reader.ReadString('\n')
-	deleteChoice = strings.TrimSpace(deleteChoice)
+	fmt.Scan(&deleteChoice)
+
 	if deleteChoice != "yes" {
 		fmt.Println("Exiting without deleting any files.")
 		os.Exit(0)
 	}
-	for hash, files := range fileHashes {
+
+	for hash, files := range hashes {
 		if len(files) > 1 {
-			fmt.Printf("Duplicates for hash %x:\n", md5.Sum([]byte(files[0])))
+			fmt.Printf("Duplicates for hash %s:\n", hash)
 			fmt.Println("0) Keep all duplicates")
 			for i, file := range files {
 				fmt.Printf("%d) %s\n", i+1, file)
 			}
-			var keepChoice int
+
+			var keepChoice string
 			for {
-				fmt.Print("Enter the number of the file you wish to keep: ")
-				_, err := fmt.Fscan(reader, &keepChoice)
-				if err != nil {
-					fmt.Fprintln(os.Stderr, "Error reading input:", err)
-					continue
-				}
-				if keepChoice == 0 {
-					fmt.Printf("Keeping all duplicate files for hash %x.\n", hash)
+				fmt.Print("Enter the number of the file you wish to keep, or 'd' to delete all: ")
+				fmt.Scan(&keepChoice)
+
+				if keepChoice == "d" {
+					for _, file := range files {
+						fmt.Printf("Deleting: %s\n", file)
+						os.Remove(file)
+					}
+					fmt.Println("All duplicates have been deleted.")
 					break
-				} else if keepChoice > 0 && keepChoice <= len(files) {
-					keepIndex := keepChoice - 1
+				} else if keep, err := strconv.Atoi(keepChoice); err == nil && keep == 0 {
+					fmt.Printf("Keeping all duplicate files for hash %s.\n", hash)
+					break
+				} else if keep > 0 && keep <= len(files) {
+					keepIndex := keep - 1
 					for i, file := range files {
 						if i != keepIndex {
-							fmt.Println("Deleting:", file)
-							err := os.Remove(file)
-							if err != nil {
-								fmt.Fprintf(os.Stderr, "Failed to delete file %s: %v\n", file, err)
-							}
+							fmt.Printf("Deleting: %s\n", file)
+							os.Remove(file)
 						}
 					}
-					fmt.Println("Kept:", files[keepIndex])
+					fmt.Printf("Kept: %s\n", files[keepIndex])
 					break
 				} else {
-					fmt.Println("Invalid selection. Please choose a valid file number.")
+					fmt.Println("Invalid selection. Please choose a valid file number or 'd' to delete all.")
 				}
 			}
 			fmt.Println()
 		}
 	}
+
 	fmt.Println("Duplicate files deletion complete.")
+}
+
+func fileMD5(filePath string) (string, error) {
+	var returnMD5String string
+	file, err := os.Open(filePath)
+	if err != nil {
+		return returnMD5String, err
+	}
+	defer file.Close()
+
+	hash := md5.New()
+	if _, err := io.Copy(hash, file); err != nil {
+		return returnMD5String, err
+	}
+	hashInBytes := hash.Sum(nil)[:16]
+	returnMD5String = hex.EncodeToString(hashInBytes)
+
+	return returnMD5String, nil
 }
